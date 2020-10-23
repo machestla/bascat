3f4a9c3a4980172a99ed2db6a1c931a08d774d8c
Marie-Hélène <mariehelene.rots@gmail.com>
2020-10-23 17:10:56 +0200

    y generate

---
+++ dist/_nuxt/b0506da.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{377:function(n,t,e){"use strict";e.r(t);var c=e(35),component=Object(c.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container"},[this._v("\n\n  user page\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
